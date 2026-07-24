de15850f3d6136af3010802dc4e6193bc389a223
Kamal Joshi <kamaljoshi2206@gmail.com>
2026-07-24 17:16:13 +0530

    Added scroll to top component with main layout

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 
 import Navbar from "../../components/common/Navbar";
 import Footer from "../../components/common/Footer";
+import ScrollToTop from "../../components/common/ScrollToTop";
 
 const MainLayout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-white">
+      <ScrollToTop />
       <Navbar />
 
       <main className="flex-1">
